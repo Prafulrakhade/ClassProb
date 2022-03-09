dc4c9ad7954dd0259b3fd30ddf3cc30d5916f5a6
Praful <prafulrakhade02@gmail.com>
2022-03-09 18:39:56 +0530

    [praful] add: UC1 File

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+     
+        public static string TakeUsername()
+        {
+            string name;
+            Console.Write("enter the user Name : ");
+            name = Console.ReadLine();
+            return name;
+        }
+        static void Main(string[] args)
+        {
+            string name1;
+            name1 = TakeUsername();
+            Console.WriteLine("Hello My Name is " + name1);
+        }
+    }
+}
