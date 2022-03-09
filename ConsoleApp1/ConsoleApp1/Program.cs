6a0a3a34e74aecc38fde0a29af88ea0aef60798e
Praful <prafulrakhade02@gmail.com>
2022-03-09 18:48:19 +0530

    [praful] add: UC2 File

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,11 +12,21 @@ namespace ConsoleApp1
             name = Console.ReadLine();
             return name;
         }
+        public static int TakeUserAge()
+        {
+            int age;
+            Console.WriteLine("enter the user Age");
+            age = Convert.ToInt32(Console.ReadLine());
+            return age;
+        }
         static void Main(string[] args)
         {
             string name1;
             name1 = TakeUsername();
             Console.WriteLine("Hello My Name is " + name1);
+            int age1;
+            age1 = TakeUserAge();
+            Console.WriteLine("And My Age Is "+ age1 );
         }
     }
 }
